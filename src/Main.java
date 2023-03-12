d1a11e7574f9fc21e7e514db6fe61a5ad36949a0
AdrianaB <adrianamorar94@yahoo.com>
2023-03-12 22:18:09 +0000

    Homework Bottle

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Bottle bottle = new Bottle(76, false , true);
+        bottle.isOpen();
+        bottle.isClosed();
+        bottle.availableLiquid();
+        bottle.emptyCapacity();
+        bottle.openBottle();
+        bottle.closesBottle();
+
+
+    }
+}
